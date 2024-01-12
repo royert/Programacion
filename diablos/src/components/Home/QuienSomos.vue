c82b54248784e6ce91318604463debb209c961d0
Roger Romero <royertromero1203@gmail.com>
2024-01-12 07:29:31 -0400

    yo

--- diablos/src/components/Home/QuienSomos.vue
+++ diablos/src/components/Home/QuienSomos.vue
@@ -1,16 +1,6 @@
 <template>
    <div class="container-data">
-     <!--  <div class="container-data__left">
-          <span class="container-data__left__span">EDIC HUMBERTO ALVAREZ</span> 
-         <h3 class="container-data__left__title">EDIC HUMBERTO ALVAREZ</h3>
-         <p class="container-data__left__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            velit, itaque et, consequatur vitae laborum rerum ipsum neque, autem
-            sint beatae eligendi accusantium voluptatibus dolorem. Alias autem
-            quaerat neque voluptatum.
-         </p>
-         <V-Button Texto = "Contacto" />
-      </div> -->
+     
       <div class="container-data__right">
         <img src="../../assets/banner-humbero-alvarez.png" alt="Banner">
         <V-Button Texto = "Contacto" />
